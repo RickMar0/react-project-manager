3c15b05834c3182c9b06f50441ae80c01b70a70a
RickMar0 <165326554+RickMar0@users.noreply.github.com>
2025-06-26 00:10:35 +0200

    feat: add NoProject and Sidebar components, configure Tailwind CSS and Vite
    - Created NoProject component to display a message when no projects are available.
    - Implemented Sidebar component for project navigation with an "Add Project" button.
    - Set up Tailwind CSS for styling by adding index.css and configuring tailwind.config.js.
    - Initialized Vite with React support in vite.config.js.
    - Updated main.jsx to render the App component with React.StrictMode.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar";
+import NoProject from "./components/NoProject";
+
+function App() {
+  return (
+    <>
+      <NoProject />
+      <Sidebar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NoProject.jsx
@@ -0,0 +1,12 @@
+import noProjectsImage from '../assets/no-projects.png';
+
+export default function NoProject() {
+
+
+
+  return(
+    <>
+      <img src={noProjectsImage} alt="No projects" />
+    </>
+  );
+}
