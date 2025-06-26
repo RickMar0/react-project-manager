b062df43792ed1e8e200e7fff621e12f0cbd11db
RickMar0 <165326554+RickMar0@users.noreply.github.com>
2025-06-26 23:59:52 +0200

    - added elements in NoProject - added tailwind classes in requirements.js

--- src/components/NoProject.jsx
+++ src/components/NoProject.jsx
@@ -6,7 +6,10 @@ export default function NoProject() {
 
   return(
     <>
-      <img src={noProjectsImage} alt="No projects" />
+      <img className="w-16 h-16 object-contain mx-auto" src={noProjectsImage} alt="No projects" />
+      <h2 className="text-xl font-bold text-stone-700 my-4">No Project Selected</h2>
+      <h3 className="text-xl font-bold text-stone-500 my-4">Select a project or get started with a new one</h3>
+      <button className="text-stone-700 hover:text-stone-950">Create new project</button>
     </>
   );
 }
